f74612c353d6c25f647a6036c89de7caae195e8a
astrid thijs <r0576736@student.thomasmore.be>
2017-01-02 11:58:19 +0100

    rekenmachine in html

--- main.js
+++ main.js
@@ -4,5 +4,15 @@
  * and open the template in the editor.
  */
 
+
+var express = require('express');               //express installeren via terminal
+var bodyparser = require('body-parser');        //bodyparser installeren via terminal
+
+var app = express();                            //extensie gebruiken als variabele
+
+
+
+app.listen(123);
+
 console.log("Hello Astrid");
 
