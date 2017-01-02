ef2bed8a619dced3a66086dec7d26ffea9d2e766
astrid thijs <r0576736@student.thomasmore.be>
2017-01-02 12:23:23 +0100

    GET methode van Rekenmachine

--- main.js
+++ main.js
@@ -6,11 +6,11 @@
 
 
 var express = require('express');               //express installeren via terminal
-var bodyparser = require('body-parser');        //bodyparser installeren via terminal
+var bodyParser = require('body-parser');        //bodyparser installeren via terminal
 
 var app = express();                            //extensie gebruiken als variabele
 
-app.use (bodyparser.urlencoded({
+app.use (bodyParser.urlencoded({
        extended:true
 }));
 
@@ -18,10 +18,21 @@ app.use (bodyparser.urlencoded({
  * Dit rekenmachine bevat tekens zoals "=" die niet in de naam of in waarde van een parameter 
  * mogen worden opgenomen. Deze tekens en of spaties worden vervangen door '%' [ASCII code]. */
 
+app.use (bodyParser.json());        //ontleed json applicatie
+
+
+var result = [];
+app.get("/", function(request, response){
+        response.sendfile('/Rekenmachine.html');
+});
+
 
 
 
 app.listen(123);
+
+
+
 
 console.log("Hello Astrid");
 
